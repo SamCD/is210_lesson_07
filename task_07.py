20420060cd7efeb2bfb5f3b5ba271d4f4598a637
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-11-30 20:41:22 -0500

    Revisions

--- task_07.py
+++ task_07.py
@@ -2,7 +2,19 @@
 # -*- coding: utf-8 -*-
 """Creates addition function"""
 
+DATA = {1: 15,
+        4: 18,
+        159: 148,
+        57: 158,
+        12: 158,
+        578: 1,
+        152: 2,
+        5: 123,
+        102: 48,
+        72: 299
+        }
 
+        
 def iter_dict_funky_sum(dictarg):
 
     """
